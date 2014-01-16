5b59f4dc662c5083bd2f3cf69577a25c42737c29
Kombian <gidkom@yahoo.co.uk>
2014-01-16 22:15:43 +0000

    added pagination code

--- pagination.php
+++ pagination.php
@@ -1,32 +1,43 @@
 <?php
-//database connection
+//database connection using php pdo
 
-$database = ''; //db name
-$user = ''; //db user
-$password = ''; //db password
+$database = '';  //db name
+$user = '';      //db user
+$password = '';  //db password
 
 $dsn = "mysql:host=localhost;dbname=$database";
 $pdo = new PDO($dsn, $user, $password);
 
+//get number of entries in table
 $cnt = $pdo->query("SELECT COUNT(*) FROM tbl_name");  
 $total_pages = $cnt->fetchColumn();
 
+//current page pagination is taking place
 $targetpage = "mypage.php"; 
-$limit = 5;
+
+$limit = 5; //number of entries to show at a time
+
+//get the current pagination page
+//if its on first results set page to 1
 $page = isset($_GET['page']) ? $_GET['page']: 1;
 
+//set the start point of the queries on the table
 $start = $page ? ($page - 1)*$limit : 0;
 
+//database query based on $start parameters given
 $pds = $pdo->query("SELECT * FROM tbl_name LIMIT $start, $limit");
 
 
-//pagination
 	if($page == 0) $page = 1;
+
+	//setting of values for previous and next buttons
 	$prev = $page - 1;
 	$next = $page + 1;
+
 	$lastpage = ceil($total_pages/$limit);
-	$lpm1 = $lastpage - 1;
+
 	$pagination = "";
+	
 	if($lastpage > 1){
 	    $pagination .="<div class='pagination'>";
 	    //prev button
